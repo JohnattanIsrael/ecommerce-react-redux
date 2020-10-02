ad5981913118bae1fc91c6eb555949c8bd671deb
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-02 18:32:50 -0500

    built form input component and placed placeholders and titles passing down props to the component

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
+import { FormInput } from '../formFields'
+
 class SignInForm extends Component {
     render() {
         const { className } = this.props;
         return (
             <form className={`${className} sign-in-form`}>
-                sign inn
+                <Field
+                    className='sign-in-form__email'
+                    name='email'
+                    type='email'
+                    title='Email'
+                    placeholder='Email'
+                    component={FormInput} />
+
+                <Field
+                    className='sign-in-form__password'
+                    name='password'
+                    type='password'
+                    title='Password'
+                    placeholder='Password'
+                    component={FormInput} />
+
+                <button></button>
             </form>
         );
     }
