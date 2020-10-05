e36ed3340bda6591c7be3c5256e3f448aa951004
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-05 17:02:28 -0500

    addes basic structure for account componentsa and account route

---
+++ src/components/account/account.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Account extends Component {
+
+    renderContent() {
+        // if(this.props.navbarLinks.map)
+        return <h1>content</h1>
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.renderContent()
+                }
+            </div>
+        );
+    }
+}
+
+export default Account;
