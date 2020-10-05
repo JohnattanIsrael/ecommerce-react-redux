94ff5cbd32644c202a797611b60b25fe8db3a8b7
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-05 17:49:30 -0500

    developes navabar and header actions for settin new links and used in account.js

---
+++ src/actions/headernavbar.js
@@ -0,0 +1,20 @@
+import Navbar from "../components/headernav/navbar";
+
+import {
+    SET_HEADER_LINKS,
+    SET_NAVBAR_LINKS
+} from './types';
+
+export function setHeaderLinks (links) {
+    return ({
+        type:SET_HEADER_LINKS,
+        payload: links
+    })
+}
+
+export function setNavbarLinks (links) {
+    return ({
+        type:SET_NAVBAR_LINKS,
+        payload: links
+    })
+}
