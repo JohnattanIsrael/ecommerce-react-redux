738c080a7abfbff9e4a5e848773528b12e3c91a2
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-05 22:50:48 -0500

    finished navbar feature with help

--- src/actions/headernavbar.js
+++ src/actions/headernavbar.js
@@ -1,8 +1,7 @@
-import Navbar from "../components/headernav/navbar";
-
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from './types';
 
 export function setHeaderLinks (links) {
@@ -16,5 +15,12 @@ export function setNavbarLinks (links) {
     return ({
         type:SET_NAVBAR_LINKS,
         payload: links
+    })
+}
+
+export function changeNavbarActive (_id) {
+    return ({
+        type:CHANGE_NAVBAR_ACTIVE,
+        payload: _id
     })
 }
--- src/components/headernav/navbar.js
+++ src/components/headernav/navbar.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
+import * as actions from '../../actions';
+
 class Navbar extends Component {
     render() {
         return (
             <div className='navbar'>
                 {
-                    this.props.navbarLinks.map((link, index) => [
-                        <a className={`navbar__link ${link.active ? 'green-text' : ''}`} key={index} onClick={() => console.log('tryn to switch tab')}>
+                    this.props.navbarLinks.map((link, index) => {
+                        return(
+                        <a className={`navbar__link ${link.active ? 'green-text' : ''}`} key={index} onClick={() => this.props.changeNavbarActive(link._id)}>
                             {link.title}
                         </a>
-                    ])
+                        )
+                    })
                 }
             </div>
         );
@@ -25,6 +29,6 @@ function mapStateToProps(state) {
     }
 }
 
-Navbar = connect(mapStateToProps)(Navbar);
+Navbar = connect(mapStateToProps, actions)(Navbar);
 
 export default Navbar;
