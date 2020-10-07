d08b524ee52fd4495a488e671a90b7b48f2dbfd7
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-07 12:15:30 -0500

    developed shop reducer ad fetchShopCategories action creator, Initialy into categories state but changes to reuse the navval links state

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -15,6 +15,7 @@ class Shop extends Component {
             
         ]
         this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories();
                 // set header links
         // fetch shop products action creator
 
