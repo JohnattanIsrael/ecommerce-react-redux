a75aa0eb838525ce2205b024d4561a8715aee37d
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-08 13:34:52 -0500

    structired cart conetent products component and styled title

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -8,10 +8,19 @@ function CartButton({className, icon}) {
     )
 }
 
-function CartContent({className}) {
+function CartContent({className, products}) {
+let count = products.length;
     return (
         <div className={`${className} cart-content`}>
+            <div className='cart-content__title'>
+                Cart ({count})
+            </div>
+            <div className='cart-content__products'>
 
+            </div>
+            <div className='cart-content__footer'>
+
+            </div>
         </div>
     )
 }
@@ -27,6 +36,7 @@ export default class ShopCart extends Component {
                 />
                 <CartContent
                 className='shop-cart__content'
+                products={[4, 444, 443]}
                 />
                 
             </div>
