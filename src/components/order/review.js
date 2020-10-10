5dd9cd4d9673095ba67bf9fb7476a697593f8e68
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-09 19:51:44 -0500

    built review form buttons

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 import PageTitle from '../pageTitle';
+import ReviewForm from './reviewForm';
 
 class Review extends Component {
 
@@ -12,12 +13,20 @@ componentDidMount() {
     this.props.setNavbarLinks([]);
 }
 
+onSumbit = (fields) => {
+    console.log(fields);
+}
+
     render() {
         return (
             <div className='review'>
                 <PageTitle
                 className='review__page-title'
                 title='Order Review'
+                />
+                <ReviewForm
+                className='review__form'
+                onSumbit={this.onSumbit}
                 />
             </div>
         );
---
+++ src/components/order/reviewForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+import { FormButton } from '../formFields';
+
+import history from '../../history';
+
+class ReviewForm extends Component {
+    render() {
+        const { className, handleSubmit } = this.props;
+        
+        return (
+            <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                                
+                <div className='review-form__line'></div>
+                
+                <Field
+                    onClick={() => history.push('/account')}
+                    className='review-form__proceed'
+                    name='proceed'
+                    type='submit'
+                    title='Proceed To Checkout'
+                    component={FormButton} />
+
+                
+                <Field
+                    onClick={() => history.push('/signin')}
+                    className='review-form__back'
+                    name='back'
+                    type='button'
+                    title='Back'
+                    short={true}
+                    component={FormButton} />
+
+            </form>
+        );
+    }
+}
+
+ReviewForm = reduxForm({
+    form: 'ReviewForm'
+})(ReviewForm);
+
+export default ReviewForm;
