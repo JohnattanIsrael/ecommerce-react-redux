a0944398e908ae45261c5ec2d080a56ce4455585
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-10 17:44:01 -0500

    finished shipping form component and sub-components

--- src/components/information/orderSummary.js
+++ src/components/information/orderSummary.js
@@ -2,32 +2,51 @@ import React, { Component } from 'react';
 
 import { UnderlinedTitle, InfoTitle } from './infoHelp';
 
+import { connect } from 'react-redux';
+
 class OrderSummary extends Component {
     render() {
         const { className } = this.props;
+        let subtotal = 0;
+        let tax = 0.16;
+        let amountStickers = 0;
+        this.props.cartProducts.map(cartProduct => {
+            subtotal += cartProduct.quantity * cartProduct.product.price;
+            amountStickers += cartProduct.quantity;
+        })
         return (
             <div className={`${className} order-summary`}>
 
-                <UnderlinedTitle className='order-summary__title'/>
+                <UnderlinedTitle 
+                className='order-summary__title'
+                title='Order Summary'
+                />
 
                 <InfoTitle 
                 className='order-summary__subtotal' 
-                title='4 stickers' 
-                value='5.96' 
+                title={`${amountStickers} stickers`} 
+                value={`$${subtotal}`} 
                 />
                 <InfoTitle 
                 className='order-summary__tax-shipping' 
                 title='Taxes and Shipping' 
-                value='0.00' 
+                value={`$${tax}`}  
                 />
                 <InfoTitle 
-                className='order-summary__total' 
+                className='order-summary__total info-title-green' 
                 title='Total' 
-                value='8.02' 
+                value={`$${subtotal + tax}`}  
                 />
             </div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    const { cartProducts } = state.user;
+    return { cartProducts }
+}
+
+OrderSummary = connect(mapStateToProps)(OrderSummary);
 
 export default OrderSummary;
